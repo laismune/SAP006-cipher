e17be7ed68e8664adf1e7e619ffd29b04a88d1cb
Lais Mune <aymune@hotmail.com>
2021-06-04 19:11:06 -0300

    cipher.encode() e cipher.decode()

--- src/cipher.js
+++ src/cipher.js
@@ -1,109 +1,111 @@
 const cipher = {
-  // ...
+  encode: function (string, offset) {
+    let messageEncoded = ""
+    for (var i = 0; i < string.length; i++) {
+      let codeAsc = string.charCodeAt(i);
+      if (offset <= 0) {
+        let newValue = offset * (-1)
+        if (codeAsc <= 90 && codeAsc >=65) {
+          if (codeAsc - newValue >= 65) {
+            let newAlphabetIndex = codeAsc - newValue;
+            let newLetter = String.fromCharCode(newAlphabetIndex);
+            messageEncoded += newLetter;
+          } else {
+            let newAlphabetIndex = codeAsc - newValue + 26;
+            let newLetter = String.fromCharCode(newAlphabetIndex);
+            messageEncoded += newLetter
+          }
+        } 
+        else if (codeAsc <= 122 && codeAsc >= 97) {
+          if (codeAsc - newValue >= 97) {
+            let newAlphabetIndex = codeAsc - newValue;
+            let newLetter = String.fromCharCode(newAlphabetIndex);
+            messageEncoded += newLetter;	
+          } else {
+            let newAlphabetIndex = codeAsc - newValue + 26;
+            let newLetter = String.fromCharCode(newAlphabetIndex)
+            messageEncoded += newLetter
+          }
+        } 
+        else {
+          console.log("ERRO")
+        }		
+      } else {
+        if (codeAsc <= 90 && codeAsc >=65) {
+          let alphabetIndex = codeAsc - 65;
+          let newAlphabetIndex = (alphabetIndex + offset) % 26;
+          let newCodAsc = newAlphabetIndex + 65;
+          let newLetter = String.fromCharCode(newCodAsc);
+          messageEncoded += newLetter; 	
+        } 
+        else if (codeAsc <= 122 && codeAsc >= 97) {
+          let alphabetIndex = codeAsc - 97;
+          let newAlphabetIndex = (alphabetIndex + offset) % 26;
+          let newCodAsc = newAlphabetIndex + 97;
+          let newLetter = String.fromCharCode(newCodAsc);
+          messageEncoded += newLetter; 	
+        } 
+        else {
+        console.log("ERRO")		
+        }
+      }
+    }
+  console.log (messageDecoded)
+  },
+  decode: function (string, offset) {
+    let messageDecoded = "";
+    for (var i = 0; i < string.length; i++) {
+      let codeAsc = string.charCodeAt(i);
+      if (offset <= 0) {
+        let newValue = offset * (-1)
+        if (codeAsc <= 90 && codeAsc >=65) {
+          let alphabetIndex = codeAsc - 65;
+          let newAlphabetIndex = (alphabetIndex + newValue) % 26;
+          let newCodAsc = newAlphabetIndex + 65;
+          let newLetter = String.fromCharCode(newCodAsc);
+          messageDecoded += newLetter; 				
+        } 
+        else if (codeAsc <= 122 && codeAsc >= 97) {
+          let alphabetIndex = codeAsc - 97;
+          let newAlphabetIndex = (alphabetIndex + newValue) % 26;
+          let newCodAsc = newAlphabetIndex + 97;
+          let newLetter = String.fromCharCode(newCodAsc);
+          messageDecoded += newLetter; 					
+        } 
+        else {
+          console.log("ERRO")
+        }			
+      } else {
+        if (codeAsc <= 90 && codeAsc >=65) {
+          if (codeAsc - offset >= 65) {
+            let newAlphabetIndex = codeAsc - offset;
+            let newLetter = String.fromCharCode(newAlphabetIndex);
+            messageDecoded += newLetter;		
+          } else {
+            let newAlphabetIndex = codeAsc - offset + 26;
+            let newLetter = String.fromCharCode(newAlphabetIndex)
+            messageDecoded += newLetter
+          }			
+        } 
+        else if (codeAsc <= 122 && codeAsc >= 97) {
+          if (codeAsc - offset >= 97) {
+            let newAlphabetIndex = codeAsc - offset;
+            let newLetter = String.fromCharCode(newAlphabetIndex);
+            messageDecoded += newLetter;		
+          } else {
+            let newAlphabetIndex = codeAsc - offset + 26;
+            let newLetter = String.fromCharCode(newAlphabetIndex)
+            messageDecoded += newLetter
+          }		
+        }
+        else {
+          console.log("ERRO")		
+        }	
+      }
+    }
+  console.log (messageDecoded)
+  }
 };
 
 export default cipher;
 
-//ENCODE ANY VALUE OF OFFSET - optimized
-function encode (string, value) {
-	let messageEncoded = "";
-	for (var i = 0; i < string.length; i++) {
-		let codeAsc = string.charCodeAt(i);
-		if (value <= 0) {
-			let newValue = value * (-1)
-				if (codeAsc <= 90 && codeAsc >=65) {
-					if (codeAsc - newValue >= 65) {
-						let newAlphabetIndex = codeAsc - newValue;
-						let newLetter = String.fromCharCode(newAlphabetIndex);
-						messageEncoded += newLetter;
-					} else {
-						let newAlphabetIndex = codeAsc - newValue + 26;
-						let newLetter = String.fromCharCode(newAlphabetIndex);
-						messageEncoded += newLetter
-  						}
-  				} else if (codeAsc <= 122 && codeAsc >= 97) {
-  					if (codeAsc - newValue >= 97) {
-						let newAlphabetIndex = codeAsc - newValue;
-						let newLetter = String.fromCharCode(newAlphabetIndex);
-						messageEncoded += newLetter;	
-					} else {
-						let newAlphabetIndex = codeAsc - newValue + 26;
-						let newLetter = String.fromCharCode(newAlphabetIndex)
-						messageEncoded += newLetter
-						}
-  				} else {
-  					console.log("ERRO")
-  					}		
-		} else {
-			if (codeAsc <= 90 && codeAsc >=65) {
- 				let alphabetIndex = codeAsc - 65;
-  				let newAlphabetIndex = (alphabetIndex + value) % 26;
-  				let newCodAsc = newAlphabetIndex + 65;
-  				let newLetter = String.fromCharCode(newCodAsc);
-  				messageEncoded += newLetter; 	
-  			} else if (codeAsc <= 122 && codeAsc >= 97) {
-  				let alphabetIndex = codeAsc - 97;
-  				let newAlphabetIndex = (alphabetIndex + value) % 26;
-  				let newCodAsc = newAlphabetIndex + 97;
-  				let newLetter = String.fromCharCode(newCodAsc);
-  				messageEncoded += newLetter; 	
-  			} else {
-  				console.log("ERRO")
-				}		
-		}
-	}
-console.log(messageEncoded)
-}
-
-
-//#DECODE ANY VALUE OF OFFSET - optimized
-function decode (string, value) {
-	let messageDecoded = "";
-	for (var i = 0; i < string.length; i++) {
-		let codeAsc = string.charCodeAt(i);
-		if (value <= 0) {
-			let newValue = value * (-1)
-			if (codeAsc <= 90 && codeAsc >=65) {
- 				let alphabetIndex = codeAsc - 65;
-  				let newAlphabetIndex = (alphabetIndex + newValue) % 26;
-  				let newCodAsc = newAlphabetIndex + 65;
-  				let newLetter = String.fromCharCode(newCodAsc);
-  				messageDecoded += newLetter; 				
-  			} else if (codeAsc <= 122 && codeAsc >= 97) {
-  				let alphabetIndex = codeAsc - 97;
-  				let newAlphabetIndex = (alphabetIndex + newValue) % 26;
-  				let newCodAsc = newAlphabetIndex + 97;
-  				let newLetter = String.fromCharCode(newCodAsc);
-  				messageDecoded += newLetter; 					
-  			} else {
-  				console.log("ERRO")
-  				}			
-  		} else {
-			if (codeAsc <= 90 && codeAsc >=65) {
-			
-				if (codeAsc - value >= 65) {
-					let newAlphabetIndex = codeAsc - value;
-					let newLetter = String.fromCharCode(newAlphabetIndex);
-					messageDecoded += newLetter;		
-				} else {
-					let newAlphabetIndex = codeAsc - value + 26;
-					let newLetter = String.fromCharCode(newAlphabetIndex)
-					messageDecoded += newLetter
-  					}			
-  			} else if (codeAsc <= 122 && codeAsc >= 97) {
-  				if (codeAsc - value >= 97) {
-					let newAlphabetIndex = codeAsc - value;
-					let newLetter = String.fromCharCode(newAlphabetIndex);
-					messageDecoded += newLetter;		
-				} else {
-					let newAlphabetIndex = codeAsc - value + 26;
-					let newLetter = String.fromCharCode(newAlphabetIndex)
-					messageDecoded += newLetter
-  					}		
-  			} else {
-  				console.log("ERRO")		
-  				}	
-  			}
-		}
-console.log(messageDecoded)
-}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let string = ""
+let offset = ""
+
+string = document.getElementById("string").value;
+offset = document.getElementById("offset").value;
+
+document.write(cipher.encode(string,offset).value)
