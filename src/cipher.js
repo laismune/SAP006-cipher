f96be8061694d9c36ea583fbae2db680f2762218
Lais Mune <aymune@hotmail.com>
2021-06-08 11:27:00 -0300

    Correção do JS - offset negativo para qualquer valor está funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,119 +1,157 @@
 const cipher = {
-encode:function (offset, string) {
-  let offsetValue = parseInt(offset,)
+  encode:function (offset, string) {
   let messageEncoded = "";
-  for (var i = 0; i < string.length; i++) {
-    let codeAsc = string.charCodeAt(i);
-    if (offsetValue <= 0) {
-      let newValue = offsetValue * (-1)
-        if (codeAsc <= 90 && codeAsc >=65) {
-          if (codeAsc - newValue >= 65) {
-            let newAlphabetIndex = codeAsc - newValue;
-            let newLetter = String.fromCharCode(newAlphabetIndex);
-            messageEncoded += newLetter;
-          } else {
-            let newAlphabetIndex = codeAsc - newValue + 26;
-            let newLetter = String.fromCharCode(newAlphabetIndex);
-            messageEncoded += newLetter
-          }
-        } 
-        else if (codeAsc <= 122 && codeAsc >= 97) {
-          if (codeAsc - newValue >= 97) {
-            let newAlphabetIndex = codeAsc - newValue;
-            let newLetter = String.fromCharCode(newAlphabetIndex);
-            messageEncoded += newLetter;	
-          } else {
-            let newAlphabetIndex = codeAsc - newValue + 26;
-            let newLetter = String.fromCharCode(newAlphabetIndex)
-            messageEncoded += newLetter
-          }
-        } 
-        else {
-          let newAlphabetIndex = codeAsc;	
-          let newLetter = String.fromCharCode(newAlphabetIndex);	
-          messageEncoded += newLetter;
-          }		
-    } else {
-      if (codeAsc <= 90 && codeAsc >=65) {
-        let alphabetIndex = codeAsc - 65;
-        let newAlphabetIndex = (alphabetIndex + offsetValue) % 26;
-        let newCodAsc = newAlphabetIndex + 65;
-        let newLetter = String.fromCharCode(newCodAsc);
-        messageEncoded += newLetter; 	
-      } 
-      else if (codeAsc <= 122 && codeAsc >= 97) {
-        let alphabetIndex = codeAsc - 97;
-        let newAlphabetIndex = (alphabetIndex + offsetValue) % 26;
-        let newCodAsc = newAlphabetIndex + 97;
-        let newLetter = String.fromCharCode(newCodAsc);
-        messageEncoded += newLetter; 	
-      } else {
-        let newAlphabetIndex = codeAsc;	
-        let newLetter = String.fromCharCode(newAlphabetIndex);	
-        messageEncoded += newLetter;
-      }		
-    }
-  }
-  return messageEncoded;
-},
-decode: function (offset, string) {
-  let offsetValue = parseInt(offset, 10)
+	let valueNumber = parseInt(offset, 10)
+	for (var i = 0; i < string.length; i++) {
+		let codeAsc = string.charCodeAt(i);
+		if (valueNumber >=0) {
+			if (codeAsc <= 90 && codeAsc >=65) {
+				let alphabetIndex = codeAsc - 65;
+				let newAlphabetIndex = (alphabetIndex + valueNumber) % 26;
+				let newCodAsc = newAlphabetIndex + 65;
+				let newLetter = String.fromCharCode(newCodAsc);
+				messageEncoded += newLetter; 	
+			} 
+			else if (codeAsc <= 122 && codeAsc >= 97) {
+				let alphabetIndex = codeAsc - 97;
+				let newAlphabetIndex = (alphabetIndex + valueNumber) % 26;
+				let newCodAsc = newAlphabetIndex + 97;
+				let newLetter = String.fromCharCode(newCodAsc);
+				messageEncoded += newLetter; 	
+			} else {
+				let newAlphabetIndex = codeAsc;	
+				let newLetter = String.fromCharCode(newAlphabetIndex);	
+				messageEncoded += newLetter; 
+				}
+		} else {
+			if (codeAsc <= 90 && codeAsc >=65) {
+				let position = codeAsc - 65;
+				if (position + valueNumber > 0) {
+					let newPosition = position + valueNumber;
+					let newAlphabetIndex = newPosition + 65;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageEncoded += newLetter;
+				} 
+				else if (position + valueNumber < 0) {
+					let newPosition = position + valueNumber;
+					let newPositionPositive = newPosition * (-1)
+					let newPositionFull = newPositionPositive % 26;
+					let newAlphabetIndex = (26 - newPositionFull + 65);
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageEncoded += newLetter;
+				} else {
+					let newAlphabetIndex = 65;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageEncoded += newLetter;
+				}
+			} 
+			else if (codeAsc <= 122 && codeAsc >=97) {
+				let position = codeAsc - 97;
+				if (position + valueNumber > 0) {
+					let newPosition = position + valueNumber;
+					let newAlphabetIndex = newPosition + 97;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageEncoded += newLetter;
+				} 
+				else if (position + valueNumber < 0) {
+					let newPosition = position + valueNumber;
+					let newPositionPositive = newPosition * (-1)
+					let newPositionFull = newPositionPositive % 26;
+					let newAlphabetIndex = (26 - newPositionFull + 97);
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageEncoded += newLetter;
+				} else {
+					let newAlphabetIndex = 97;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageEncoded += newLetter;
+				}
+			} else {
+				let newAlphabetIndex = codAsc;
+				let newLetter = String.fromCharCode(newAlphabetIndex);
+				messageEncoded += newLetter
+			}
+		}
+	}
+  return messageEncoded		
+  },
+  decode: function (offset, string) {
   let messageDecoded = "";
-  for (var i = 0; i < string.length; i++) {
-    let codeAsc = string.charCodeAt(i);
-    if (offsetValue <= 0) {
-      let newValue = offsetValue * (-1)
-      if (codeAsc <= 90 && codeAsc >=65) {
-        let alphabetIndex = codeAsc - 65;
-        let newAlphabetIndex = (alphabetIndex + newValue) % 26;
-        let newCodAsc = newAlphabetIndex + 65;
-        let newLetter = String.fromCharCode(newCodAsc);
-        messageDecoded += newLetter; 				
-      } 
-      else if (codeAsc <= 122 && codeAsc >= 97) {
-        let alphabetIndex = codeAsc - 97;
-        let newAlphabetIndex = (alphabetIndex + newValue) % 26;
-        let newCodAsc = newAlphabetIndex + 97;
-        let newLetter = String.fromCharCode(newCodAsc);
-        messageDecoded += newLetter; 					
-      } 
-      else {
-        let newAlphabetIndex = codeAsc;	
-        let newLetter = String.fromCharCode(newAlphabetIndex);	
-        messageDecoded += newLetter;
-      }			
-    } else {
-      if (codeAsc <= 90 && codeAsc >=65) {
-        if (codeAsc - offsetValue >= 65) {
-          let newAlphabetIndex = codeAsc - offsetValue;
-          let newLetter = String.fromCharCode(newAlphabetIndex);
-          messageDecoded += newLetter;		
-        } else {
-          let newAlphabetIndex = codeAsc - offsetValue + 26;
-          let newLetter = String.fromCharCode(newAlphabetIndex)
-          messageDecoded += newLetter
-        }			
-      } 
-      else if (codeAsc <= 122 && codeAsc >= 97) {
-        if (codeAsc - offsetValue >= 97) {
-          let newAlphabetIndex = codeAsc - ooffsetValue;
-          let newLetter = String.fromCharCode(newAlphabetIndex);
-          messageDecoded += newLetter;		
-        } else {
-          let newAlphabetIndex = codeAsc - offsetValue + 26;
-          let newLetter = String.fromCharCode(newAlphabetIndex)
-          messageDecoded += newLetter
-          }		
-      } 
-      else {
-        let newAlphabetIndex = codeAsc;	
-        let newLetter = String.fromCharCode(newAlphabetIndex);	
-        messageDecoded += newLetter;
-        }	
-      }
+	let valueNumber = parseInt(offset, 10)
+	for (var i = 0; i < string.length; i++) {
+		let codeAsc = string.charCodeAt(i);
+		if (valueNumber >=0) {
+			let valueNumbeToDecode = valueNumber * (-1)
+			if (codeAsc <= 90 && codeAsc >=65) {
+				let position = codeAsc - 65;
+				if (position + valueNumbeToDecode > 0) {
+					let newPosition = position + valueNumbeToDecode;
+					let newAlphabetIndex = newPosition + 65;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageDecoded += newLetter;
+				} 
+				else if (position + valueNumbeToDecode < 0) {
+					let newPosition = position + valueNumbeToDecode;
+					let newPositionPositive = newPosition * (-1)
+					let newPositionFull = newPositionPositive % 26;
+					let newAlphabetIndex = (26 - newPositionFull + 65);
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageDecoded += newLetter;
+				} else {
+					let newAlphabetIndex = 65;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageDecoded += newLetter;
+				}
+			} 
+			else if (codeAsc <= 122 && codeAsc >=97) {
+				let position = codeAsc - 97;
+				if (position + valueNumbeToDecode > 0) {
+					let newPosition = position + valueNumbeToDecode;
+					let newAlphabetIndex = newPosition + 97;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageDecoded += newLetter;
+				} 
+				else if (position + valueNumbeToDecode < 0) {
+					let newPosition = position + valueNumbeToDecode;
+					let newPositionPositive = newPosition * (-1)
+					let newPositionFull = newPositionPositive % 26;
+					let newAlphabetIndex = (26 - newPositionFull + 97);
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageDecoded += newLetter;
+				} else {
+					let newAlphabetIndex = 97;
+					let newLetter = String.fromCharCode(newAlphabetIndex);
+					messageDecoded += newLetter;
+				}
+			} else {
+				let newAlphabetIndex = codAsc;
+				let newLetter = String.fromCharCode(newAlphabetIndex);
+				messageDecoded += newLetter
+			}
+		} else {
+			let valueNumbeToDecode = valueNumber * (-1);
+			if (codeAsc <= 90 && codeAsc >=65) {
+				let alphabetIndex = codeAsc - 65;
+				let newAlphabetIndex = (alphabetIndex + valueNumbeToDecode) % 26;
+				let newCodAsc = newAlphabetIndex + 65;
+				let newLetter = String.fromCharCode(newCodAsc);
+				messageDecoded += newLetter; 	
+			} 
+			else if (codeAsc <= 122 && codeAsc >= 97) {
+				let alphabetIndex = codeAsc - 97;
+				let newAlphabetIndex = (alphabetIndex + valueNumbeToDecode) % 26;
+				let newCodAsc = newAlphabetIndex + 97;
+				let newLetter = String.fromCharCode(newCodAsc);
+				messageDecoded += newLetter; 	
+			} else {
+				let newAlphabetIndex = codeAsc;	
+				let newLetter = String.fromCharCode(newAlphabetIndex);	
+				messageDecoded += newLetter; 
+				}
+		}
+	}
+  return messageDecoded	
   }
-  return messageDecoded;
-}
-};
-
-export default cipher;
+  };
+  
+  export default cipher;
+  
