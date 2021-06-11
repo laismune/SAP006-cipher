739e8fe3c53b1fd89c584965418218ae9a7da0e6
Lais Mune <aymune@hotmail.com>
2021-06-11 17:01:35 -0300

    Testes > 70%

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -34,12 +34,28 @@ describe('cipher', () => {
       expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
     });
 
+    it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset -33', () => {
+      expect(cipher.encode(-33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+    });
+
+    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset -33', () => {
+      expect(cipher.encode(-33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
     it('should return " !@" for " !@"', () => {
       expect(cipher.encode(33, ' !@')).toBe(' !@');
     });
+
+    it('should return " !@" for " !@"', () => {
+      expect(cipher.encode(-33, ' !@')).toBe(' !@');
+    });
+
+
+
   });
 
   describe('cipher.decode', () => {
@@ -68,12 +84,28 @@ describe('cipher', () => {
       expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
     });
 
+
+    it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset -33', () => {
+      expect(cipher.decode(-33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+    });
+
+    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset -33', () => {
+      expect(cipher.decode(-33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
     it('should return " !@" para " !@"', () => {
       expect(cipher.decode(33, ' !@')).toBe(' !@');
     });
+
+    it('should return " !@" para " !@"', () => {
+      expect(cipher.decode(-33, ' !@')).toBe(' !@');
+    });
+
+
+  
   });
 
 });
